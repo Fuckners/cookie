6ed1f4467736a51bb82d9dd7ed11a6889e12ca7b
Felipe Fuckner <100722316+Fuckners@users.noreply.github.com>
2024-01-08 15:41:10 -0300

    🐛fix: await's ausentes

--- apps/src/servers/server2/controllers/vehicle.ts
+++ apps/src/servers/server2/controllers/vehicle.ts
@@ -1,23 +1,24 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Response } from "express";
 import VehicleService from "../services/vehicle";
+import { IRequest } from "../../../@types/express";
 
 export default class VehicleController {
-  static async get(_req: Request, res: Response, next: NextFunction) {
+  static async get(_req: IRequest, res: Response, next: NextFunction) {
     try {
-      const vehicles = VehicleService.get();
+      const vehicles = await VehicleService.get();
       res.send(vehicles);
     } catch (error) {
       next(error);
     }
   }
   
-  static async getById(req: Request<{ vehicleId: number }>, res: Response, next: NextFunction) {
+  static async getById(req: IRequest<{ vehicleId: number }>, res: Response, next: NextFunction) {
     try {
       const {
         params: { vehicleId },
       } = req;
 
-      const vehicles = VehicleService.getById(vehicleId);
+      const vehicles = await VehicleService.getById(vehicleId);
       res.send(vehicles);
     } catch (error) {
       next(error);
